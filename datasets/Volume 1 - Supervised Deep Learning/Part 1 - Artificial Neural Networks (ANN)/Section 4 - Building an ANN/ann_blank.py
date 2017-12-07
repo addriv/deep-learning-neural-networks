7825968d259f23c50bb61bb80c117e058a5f3b8b
addriv <adrivero89@gmail.com>
2017-12-06 16:42:30 -0800

    Update part 1 notes

--- datasets/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann_blank.py
+++ datasets/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann_blank.py
@@ -73,3 +73,5 @@ y_pred = (y_pred > 0.5)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+# Part 4 - Evaluating, Improving, and Tuning the ANN
